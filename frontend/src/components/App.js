5df2911e6836af8f1d931eac5f0fb359eff68a6e
Rupam Sau <rupamsau0010@users.noreply.github.com>
2021-01-28 11:30:10 +0530

    CoursesPage complete

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import IndexPage from "./IndexPage"
 import ServicesPage from "./ServicesPage"
 import SkillsPage from "./SkillsPage"
+import CoursesPage from "./CoursesPage"
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route exact path="/" component={IndexPage} />
           <Route exact path="/services" component={ServicesPage} />
           <Route exact path="/skills" component={SkillsPage} />
+          <Route exact path="/courses" component={CoursesPage} />
         </Switch>
       </div>
     </Router>
